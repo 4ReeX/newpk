a638cb3b06292e5905e69ae01186beced1883f8a
4ReeX <56444853+4ReeX@users.noreply.github.com>
2020-04-21 17:42:03 +0300

    123

--- popup-mass-edit.php
+++ popup-mass-edit.php
@@ -37,13 +37,53 @@
                         </div>
                         </div>
                         </div></div>
+
+                        </div>
+                        <div class="rw-multiselect rw-widget float-left"><span class="rw-sr"></span>
+                        <div class="rw-widget-input rw-widget-picker rw-widget-container">
+                        <div class="jss8">
+                        <div class=" css-2b097c-container">
+                        <div class="MuiFormControl-root MuiTextField-root MuiFormControl-fullWidth">
+                        <label class="MuiFormLabel-root MuiInputLabel-root MuiInputLabel-formControl MuiInputLabel-animated MuiInputLabel-shrink" data-shrink="true" for="react-select-multiple">Список партнёров</label>
+                        <div class="MuiInputBase-root MuiInput-root MuiInput-underline MuiInputBase-fullWidth MuiInput-fullWidth MuiInputBase-formControl MuiInput-formControl">
+                        <div aria-invalid="false" class="MuiInputBase-input MuiInput-input jss9" type="text">
+                        <div class="jss10">
+                        <div class=" css-1uccc91-singleValue">Не выбрано(вниз тянется)</div>
+                        <div class="css-1tna5xz">
+                        <div class="" style="display: inline-block;">
+                        <input autocapitalize="none" autocomplete="off" autocorrect="off" id="react-select-multiple" spellcheck="false" tabindex="0" type="text" aria-autocomplete="list" value="" style="box-sizing: content-box; width: 2px; background: 0px center; border: 0px; font-size: inherit; opacity: 0; outline: 0px; padding: 0px; color: inherit;">
+                        <div style="position: absolute; top: 0px; left: 0px; visibility: hidden; height: 0px; overflow: scroll; white-space: pre; font-size: 14.4px; font-family: Roboto, Helvetica, Arial, sans-serif; font-weight: 400; font-style: normal; letter-spacing: normal; text-transform: none;"></div>
+                        </div>
+                        </div>
+                        </div>
+                        <div class=" css-1wy0on6"><span class=" css-1okebmr-indicatorSeparator"></span>
+                        <div aria-hidden="true" class=" css-tlfecz-indicatorContainer">
+                        <svg height="20" width="20" viewBox="0 0 20 20" aria-hidden="true" focusable="false" class="css-19bqh2r">
+                        <path d="M4.516 7.548c0.436-0.446 1.043-0.481 1.576 0l3.908 3.747 3.908-3.747c0.533-0.481 1.141-0.446 1.574 0 0.436 0.445 0.408 1.197 0 1.615-0.406 0.418-4.695 4.502-4.695 4.502-0.217 0.223-0.502 0.335-0.787 0.335s-0.57-0.112-0.789-0.335c0 0-4.287-4.084-4.695-4.502s-0.436-1.17 0-1.615z"></path>
+                        </svg>
+                        </div>
+                        </div>
+                        </div>
+                        </div>
+                        </div>
+                        </div>
+                        </div></div>
+                        
                         </div>
                         <div class="modal-cb">
                             <div class="modal50"> 
-                            <div class="cb-block">
-                                <label for="hideForNovice" class="float-left" style="width: 100%;">Скрыть от новичков</label>
-                                <div class="ios-checkbox-wrap"><input type="checkbox" id="hideForNovice" class="ios ios-checkbox-input" data-ios-checkbox="true" style="display: none;"></div>
-                            </div>
+                                    <label for="RadioOfferHide" class="float-left" style="width: 100%;">Скрыть от новичков</label>
+                                    <div class="btn-group btn-group-toggle" data-toggle="buttons" id="RadioOfferHide">
+                                    <label class="btn btn-secondary">
+                                    <input type="radio" name="options" id="option1" autocomplete="off" checked> Да
+                                    </label>
+                                    <label class="btn btn-secondary">
+                                    <input type="radio" name="options" id="option2" autocomplete="off"> Нет
+                                    </label>
+                                    <label class="btn btn-secondary  active">
+                                    <input type="radio" name="options" id="option3" autocomplete="off"> Без изменений
+                                    </label>
+                                    </div>
                         </div>
                         <div class="modal50"> 
                             <div class="cb-block">
@@ -52,6 +92,7 @@
                             </div>
                         </div>
                         </div>
+
                         
 
                         
@@ -66,9 +107,11 @@
                     <div class="form-group flex-start">
                          <label for="fixa" class="float-left">Ф.Ставка</label><div id="fixa" class="form-control float-left" contenteditable=true></div>
                     </div>
-                    <div class="form-group flex-start last-mb">
+                    <div class="form-group flex-start">
                          <label for="opisanie" class="float-left">Ограничения/Таргетинг</label><div id="opisanie" class="form-control float-left" contenteditable=true></div>
                     </div>
+
+                    <div class="form-group flex-start  last-mb"><div class="mr-2 flex-start modal-grid" id="notific2"><div class="modal50"><label class="float-left" style="margin-right: 50px;">Пересчитать статистику за сегодняшний день</label><div class="ios-ui-select"><div class="inner"></div></div></div><div class="modal50"><label class="float-left" style="margin-right: 58px;">Оповестить партнеров об изменении ставок </label><div class="ios-ui-select"><div class="inner"></div></div></div></div></div>
             
 
                          
