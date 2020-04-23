b79e07fa891425da6d87199a7622675e675541fb
4ReeX <56444853+4ReeX@users.noreply.github.com>
2020-04-23 16:10:16 +0300

    123

--- header.php
+++ header.php
@@ -8,7 +8,7 @@
             </div>
             <div class="mob-box">
          <span class="balance">10 565 665 <i class="fa fa-rub"></i></span>
-      <div class="icon-notification"><i class="ti ti-bell"><span class="count">13</span></i></div>
+      <div class="icon-notification"><i class="ti ti-comments"><span class="count">13</span></i></div>
       <button class="mobile chat-activator pulse" style="position: relative;"><i class="ti ti-comments"></i></button>
       <div class="avatar" onclick="goToUserPhp();"><li class="nav-item"><a data-flag="pages" href="user.php"><i class="ti ti-user"></i></a></li></div>
       </div>
