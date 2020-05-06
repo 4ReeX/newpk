a3c934eff1b05c5dd663cc4300d32447a4a75d06
4ReeX <56444853+4ReeX@users.noreply.github.com>
2020-05-06 18:53:35 +0300

    22

--- index.php
+++ index.php
@@ -152,6 +152,54 @@ include 'header.php';  //любой файл который мы хотим по
         </div>
     </div>
 </li>
+<li class="border-bottom-0 list-group-item">
+    <div class="widget-content p-0">
+        <div class="widget-content-wrapper">
+            <div class="widget-content-left mr-3"><img alt="" class="rounded-circle" src="http://newpk.skrom.dev-04.affimob.pro/images/beeline.png" width="38"></div>
+            <div class="widget-content-left">
+                <div class="widget-heading">11458 | MTS Tvmood 25/1</div>
+                <div class="widget-subheading mt-1 opacity-10">
+                    <div class="badge badge-pill badge-success" title="доход р.">9856₽</div>
+                </div>
+            </div>
+            <div class="widget-content-right">
+                <div class="fsize-2 text-success"><small class="opacity-5 pr-1">% </small> 5.0 <small class="text-muted pl-2"><i class="ti ti-angle-up"></i></small></div>
+            </div>
+        </div>
+    </div>
+</li>
+<li class="border-bottom-0 list-group-item">
+    <div class="widget-content p-0">
+        <div class="widget-content-wrapper">
+            <div class="widget-content-left mr-3"><img alt="" class="rounded-circle" src="http://newpk.skrom.dev-04.affimob.pro/images/beeline.png" width="38"></div>
+            <div class="widget-content-left">
+                <div class="widget-heading">11458 | MTS Tvmood 25/1</div>
+                <div class="widget-subheading mt-1 opacity-10">
+                    <div class="badge badge-pill badge-success" title="доход р.">9856₽</div>
+                </div>
+            </div>
+            <div class="widget-content-right">
+                <div class="fsize-2 text-success"><small class="opacity-5 pr-1">% </small> 5.0 <small class="text-muted pl-2"><i class="ti ti-angle-up"></i></small></div>
+            </div>
+        </div>
+    </div>
+</li>
+<li class="border-bottom-0 list-group-item">
+    <div class="widget-content p-0">
+        <div class="widget-content-wrapper">
+            <div class="widget-content-left mr-3"><img alt="" class="rounded-circle" src="http://newpk.skrom.dev-04.affimob.pro/images/beeline.png" width="38"></div>
+            <div class="widget-content-left">
+                <div class="widget-heading">11458 | MTS Tvmood 25/1</div>
+                <div class="widget-subheading mt-1 opacity-10">
+                    <div class="badge badge-pill badge-success" title="доход р.">9856₽</div>
+                </div>
+            </div>
+            <div class="widget-content-right">
+                <div class="fsize-2 text-success"><small class="opacity-5 pr-1">% </small> 5.0 <small class="text-muted pl-2"><i class="ti ti-angle-up"></i></small></div>
+            </div>
+        </div>
+    </div>
+</li>
 
 <li class="border-bottom-0 list-group-item">
     <div class="widget-content p-0">
