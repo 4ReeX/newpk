55fd945e7012afac05c0bef4501b67edea4e8538
4ReeX <56444853+4ReeX@users.noreply.github.com>
2020-05-28 13:57:00 +0300

    1231234

--- dinamica.php
+++ dinamica.php
@@ -12,9 +12,9 @@
                                     <div class="widget-content-left mr-3"><img alt="" class="rounded-circle" src="http://newpk.skrom.dev-04.affimob.pro/images/beeline.png" width="38" /></div>
                                     <div class="widget-content-left">
                                         <div class="widget-heading">11458 | MTS Tvmood 25/1</div>
-                                        <div class="widget-subheading mt-1 opacity-10">
+                                        <!--<div class="widget-subheading mt-1 opacity-10">
                                             <div class="badge badge-pill badge-success" title="доход р.">9856₽</div>
-                                        </div>
+                                        </div>-->
                                     </div>
                                     <div class="widget-content-right">
                                         <div class="fsize-2 text-success">
@@ -30,9 +30,6 @@
                                     <div class="widget-content-left mr-3"><img alt="" class="rounded-circle" src="http://newpk.skrom.dev-04.affimob.pro/images/beeline.png" width="38" /></div>
                                     <div class="widget-content-left">
                                         <div class="widget-heading">11458 | MTS Tvmood 25/1</div>
-                                        <div class="widget-subheading mt-1 opacity-10">
-                                            <div class="badge badge-pill badge-success" title="доход р.">9856₽</div>
-                                        </div>
                                     </div>
                                     <div class="widget-content-right">
                                         <div class="fsize-2 text-success">
@@ -48,9 +45,6 @@
                                     <div class="widget-content-left mr-3"><img alt="" class="rounded-circle" src="http://newpk.skrom.dev-04.affimob.pro/images/beeline.png" width="38" /></div>
                                     <div class="widget-content-left">
                                         <div class="widget-heading">11458 | MTS Tvmood 25/1</div>
-                                        <div class="widget-subheading mt-1 opacity-10">
-                                            <div class="badge badge-pill badge-success" title="доход р.">9856₽</div>
-                                        </div>
                                     </div>
                                     <div class="widget-content-right">
                                         <div class="fsize-2 text-success">
@@ -66,9 +60,126 @@
                                     <div class="widget-content-left mr-3"><img alt="" class="rounded-circle" src="http://newpk.skrom.dev-04.affimob.pro/images/beeline.png" width="38" /></div>
                                     <div class="widget-content-left">
                                         <div class="widget-heading">11458 | MTS Tvmood 25/1</div>
-                                        <div class="widget-subheading mt-1 opacity-10">
-                                            <div class="badge badge-pill badge-success" title="доход р.">9856₽</div>
+                                    </div>
+                                    <div class="widget-content-right">
+                                        <div class="fsize-2 text-success">
+                                            <small class="opacity-5 pr-1">% </small> 5.0 <small class="text-muted pl-2"><i class="ti ti-angle-up"></i></small>
                                         </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="border-bottom-0 list-group-item">
+                            <div class="widget-content p-0">
+                                <div class="widget-content-wrapper">
+                                    <div class="widget-content-left mr-3"><img alt="" class="rounded-circle" src="http://newpk.skrom.dev-04.affimob.pro/images/beeline.png" width="38" /></div>
+                                    <div class="widget-content-left">
+                                        <div class="widget-heading">11458 | MTS Tvmood 25/1</div>
+                                    </div>
+                                    <div class="widget-content-right">
+                                        <div class="fsize-2 text-success">
+                                            <small class="opacity-5 pr-1">% </small> 5.0 <small class="text-muted pl-2"><i class="ti ti-angle-up"></i></small>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="border-bottom-0 list-group-item">
+                            <div class="widget-content p-0">
+                                <div class="widget-content-wrapper">
+                                    <div class="widget-content-left mr-3"><img alt="" class="rounded-circle" src="http://newpk.skrom.dev-04.affimob.pro/images/beeline.png" width="38" /></div>
+                                    <div class="widget-content-left">
+                                        <div class="widget-heading">11458 | MTS Tvmood 25/1</div>
+                                    </div>
+                                    <div class="widget-content-right">
+                                        <div class="fsize-2 text-success">
+                                            <small class="opacity-5 pr-1">% </small> 5.0 <small class="text-muted pl-2"><i class="ti ti-angle-up"></i></small>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="border-bottom-0 list-group-item">
+                            <div class="widget-content p-0">
+                                <div class="widget-content-wrapper">
+                                    <div class="widget-content-left mr-3"><img alt="" class="rounded-circle" src="http://newpk.skrom.dev-04.affimob.pro/images/beeline.png" width="38" /></div>
+                                    <div class="widget-content-left">
+                                        <div class="widget-heading">11458 | MTS Tvmood 25/1</div>
+                                    </div>
+                                    <div class="widget-content-right">
+                                        <div class="fsize-2 text-success">
+                                            <small class="opacity-5 pr-1">% </small> 5.0 <small class="text-muted pl-2"><i class="ti ti-angle-up"></i></small>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="border-bottom-0 list-group-item">
+                            <div class="widget-content p-0">
+                                <div class="widget-content-wrapper">
+                                    <div class="widget-content-left mr-3"><img alt="" class="rounded-circle" src="http://newpk.skrom.dev-04.affimob.pro/images/beeline.png" width="38" /></div>
+                                    <div class="widget-content-left">
+                                        <div class="widget-heading">11458 | MTS Tvmood 25/1</div>
+                                    </div>
+                                    <div class="widget-content-right">
+                                        <div class="fsize-2 text-success">
+                                            <small class="opacity-5 pr-1">% </small> 5.0 <small class="text-muted pl-2"><i class="ti ti-angle-up"></i></small>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="border-bottom-0 list-group-item">
+                            <div class="widget-content p-0">
+                                <div class="widget-content-wrapper">
+                                    <div class="widget-content-left mr-3"><img alt="" class="rounded-circle" src="http://newpk.skrom.dev-04.affimob.pro/images/beeline.png" width="38" /></div>
+                                    <div class="widget-content-left">
+                                        <div class="widget-heading">11458 | MTS Tvmood 25/1</div>
+                                    </div>
+                                    <div class="widget-content-right">
+                                        <div class="fsize-2 text-success">
+                                            <small class="opacity-5 pr-1">% </small> 5.0 <small class="text-muted pl-2"><i class="ti ti-angle-up"></i></small>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="border-bottom-0 list-group-item">
+                            <div class="widget-content p-0">
+                                <div class="widget-content-wrapper">
+                                    <div class="widget-content-left mr-3"><img alt="" class="rounded-circle" src="http://newpk.skrom.dev-04.affimob.pro/images/beeline.png" width="38" /></div>
+                                    <div class="widget-content-left">
+                                        <div class="widget-heading">11458 | MTS Tvmood 25/1</div>
+                                    </div>
+                                    <div class="widget-content-right">
+                                        <div class="fsize-2 text-success">
+                                            <small class="opacity-5 pr-1">% </small> 5.0 <small class="text-muted pl-2"><i class="ti ti-angle-up"></i></small>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="border-bottom-0 list-group-item">
+                            <div class="widget-content p-0">
+                                <div class="widget-content-wrapper">
+                                    <div class="widget-content-left mr-3"><img alt="" class="rounded-circle" src="http://newpk.skrom.dev-04.affimob.pro/images/beeline.png" width="38" /></div>
+                                    <div class="widget-content-left">
+                                        <div class="widget-heading">11458 | MTS Tvmood 25/1</div>
+                                    </div>
+                                    <div class="widget-content-right">
+                                        <div class="fsize-2 text-success">
+                                            <small class="opacity-5 pr-1">% </small> 5.0 <small class="text-muted pl-2"><i class="ti ti-angle-up"></i></small>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="border-bottom-0 list-group-item">
+                            <div class="widget-content p-0">
+                                <div class="widget-content-wrapper">
+                                    <div class="widget-content-left mr-3"><img alt="" class="rounded-circle" src="http://newpk.skrom.dev-04.affimob.pro/images/beeline.png" width="38" /></div>
+                                    <div class="widget-content-left">
+                                        <div class="widget-heading">11458 | MTS Tvmood 25/1</div>
                                     </div>
                                     <div class="widget-content-right">
                                         <div class="fsize-2 text-success">
@@ -85,9 +196,6 @@
                                     <div class="widget-content-left mr-3"><img alt="" class="rounded-circle" src="http://newpk.skrom.dev-04.affimob.pro/images/beeline.png" width="38" /></div>
                                     <div class="widget-content-left">
                                         <div class="widget-heading">12648 | Ремембер</div>
-                                        <div class="widget-subheading mt-1 opacity-10">
-                                            <div class="badge badge-pill badge-danger" title="доход р.">4856₽</div>
-                                        </div>
                                     </div>
                                     <div class="widget-content-right">
                                         <div class="fsize-2 text-danger">
@@ -103,12 +211,9 @@
                                     <div class="widget-content-left mr-3"><img alt="" class="rounded-circle" src="http://newpk.skrom.dev-04.affimob.pro/images/beeline.png" width="38" /></div>
                                     <div class="widget-content-left">
                                         <div class="widget-heading">12648 | Ремембер</div>
-                                        <div class="widget-subheading mt-1 opacity-10">
-                                            <div class="badge badge-pill badge-warning" title="доход р.">4856₽</div>
-                                        </div>
                                     </div>
                                     <div class="widget-content-right">
-                                        <div class="fsize-2 text-warning">
+                                        <div class="fsize-2 text-danger">
                                             <small class="opacity-5 pr-1">% </small> 20.0 <small class="text-muted pl-2"><i class="ti ti-angle-down"></i></small>
                                         </div>
                                     </div>
@@ -121,9 +226,6 @@
                                     <div class="widget-content-left mr-3"><img alt="" class="rounded-circle" src="http://newpk.skrom.dev-04.affimob.pro/images/beeline.png" width="38" /></div>
                                     <div class="widget-content-left">
                                         <div class="widget-heading">12648 | Ремембер</div>
-                                        <div class="widget-subheading mt-1 opacity-10">
-                                            <div class="badge badge-pill badge-danger" title="доход р.">4856₽</div>
-                                        </div>
                                     </div>
                                     <div class="widget-content-right">
                                         <div class="fsize-2 text-danger">
@@ -139,9 +241,6 @@
                                     <div class="widget-content-left mr-3"><img alt="" class="rounded-circle" src="http://newpk.skrom.dev-04.affimob.pro/images/beeline.png" width="38" /></div>
                                     <div class="widget-content-left">
                                         <div class="widget-heading">11458 | MTS Tvmood 25/1</div>
-                                        <div class="widget-subheading mt-1 opacity-10">
-                                            <div class="badge badge-pill badge-success" title="доход р.">9856₽</div>
-                                        </div>
                                     </div>
                                     <div class="widget-content-right">
                                         <div class="fsize-2 text-success">
@@ -151,18 +250,15 @@
                                 </div>
                             </div>
                         </li>
-                        <li class="border-bottom-0 list-group-item">
+                        <li class="border-bottom-0 list-group-item  sticky-bottom">
                             <div class="widget-content p-0">
                                 <div class="widget-content-wrapper">
-                                    <div class="widget-content-left mr-3"><img alt="" class="rounded-circle" src="http://newpk.skrom.dev-04.affimob.pro/images/beeline.png" width="38" /></div>
+                                    <div class="widget-content-left mr-3"></div>
                                     <div class="widget-content-left">
-                                        <div class="widget-heading">12648 | Ремембер</div>
-                                        <div class="widget-subheading mt-1 opacity-10">
-                                            <div class="badge badge-pill badge-warning" title="доход р.">4856₽</div>
-                                        </div>
+                                        <div class="widget-heading">Выделить все офферы</div>
                                     </div>
                                     <div class="widget-content-right">
-                                        <div class="fsize-2 text-warning">
+                                        <div class="fsize-2 text-danger">
                                             <small class="opacity-5 pr-1">% </small> 20.0 <small class="text-muted pl-2"><i class="ti ti-angle-down"></i></small>
                                         </div>
                                     </div>
@@ -172,6 +268,7 @@
                     </ul>
                 </div>
                 <!-- /.card-body -->
+
             </div>
         </div>
         <!-- /.row -->
