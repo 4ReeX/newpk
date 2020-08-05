d27256fb34c05b4296ddf1842fffdd3db71655ec
4ReeX <56444853+4ReeX@users.noreply.github.com>
2020-08-05 12:09:26 +0300

    userselect+details+search popup

--- popup.php
+++ popup.php
@@ -1,71 +1,81 @@
 <style>
-	table th {    text-align: right;}
-	.modal-body {overflow-x: scroll;}
+table th {    text-align: right;}
+.modal-body {overflow-x: scroll;}
 
 </style>
 <div class="modal fade" id="smallModal" tabindex="-1" role="dialog" aria-labelledby="smallModalLabel" aria-hidden="true">
 <div class="modal-dialog modal-lg" role="document">
 <div class="modal-content">
 <div class="modal-header" style="float: left; display: flex;">
-<h5 class="modal-title"><br></h5>
+<h5 class="modal-title">Детализация кликов</h5>
+<hr>
+<div class="input-group d-flex search-line-popup">
+   <span class="search-icon"><i class="ti ti-search"></i></span>
+   <input class="form-control py-2 border-right-0 border col-lg-12" type="search" value="" placeholder="Поиск..." id="offer-search"><!--&#128270;-->
+   <span class="close" onclick="this.previousElementSibling.value = ''">
+   ×
+   </span>
+</div>
+
+
 <button type="button" class="close" data-dismiss="modal">×</button></div>
 <div class="modal-body sm-mod">
 
-                                        <table class="table  table-bordered nowrap" style="width:100%;">
-                                            <thead class="thead-light statistic_header">
-                                                <tr>
-                                                    <th class="sticky-header">ID</th>
-                                                    <th class="sticky-header">Дата,время</th>
-                                                    <th class="sticky-header">IP</th>
-                                                    <th class="sticky-header">Браузер</th>
-                                                    <th class="sticky-header">OC</th>
-                                                    <th class="sticky-header">Устройство</th>
-                                                    <th class="sticky-header">UA</th>
-                                                    <th class="sticky-header">Sub_id</th>
-                                                    <th class="sticky-header">Source</th>
+<table class="table  table-bordered nowrap details userselecttext" style="width:100%;">
+    <thead class="thead-light statistic_header">
+        <tr>
+            <th class="sticky-header">ID</th>
+            <th class="sticky-header">Дата,время</th>
+            <th class="sticky-header">IP</th>
+            <th class="sticky-header">Браузер</th>
+            <th class="sticky-header">OC</th>
+            <th class="sticky-header">Устройство</th>
+            <th class="sticky-header">UA</th>
+            <th class="sticky-header">Sub_id</th>
+            <th class="sticky-header">Source</th>
 
-                                                </tr>
-                                            </thead>
-                                            <tbody class="details_body">
-                                                <tr>
-                                                	<td>0</td>
-                                                    <td>01.06.19</td>
-                                                    <td>192.168.0.1</td>
-                                                    <td>Safary</td>
-                                                    <td>Android 4.0</td>
-                                                    <td>Mobile</td>
-                                                    <td>Mozilla</td>
-                                                    <td>123456789</td>
-                                                    <td>pornovideosite.com</td>
+        </tr>
+    </thead>
+    <tbody class="details_body">
+        <tr>
+        	<td>0</td>
+            <td>01.06.19</td>
+            <td>192.168.0.1</td>
+            <td>Safary</td>
+            <td>Android 4.0</td>
+            <td>Mobile</td>
+            <td>Mozilla</td>
+            <td>123456789</td>
+            <td>pornovideosite.com</td>
 
-                                                </tr>
-                                                <tr>
-                                                    <td>1</td>
-                                                    <td>02.06.19</td>
-                                                    <td>192.168.0.1</td>
-                                                    <td>Safary</td>
-                                                    <td>Android 4.0</td>
-                                                    <td>Mobile</td>
-                                                    <td>Mozilla</td>
-                                                    <td>123456789</td>
-                                                    <td>pornovideosite.com</td>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>02.06.19</td>
+            <td>192.168.0.1</td>
+            <td>Safary</td>
+            <td>Android 4.0</td>
+            <td>Mobile</td>
+            <td>Mozilla</td>
+            <td>123456789</td>
+            <td>pornovideosite.com</td>
 
-                                                </tr>
-                                                <tr>
-                                                    <td>2</td>
-                                                    <td>03.06.19</td>
-                                                    <td>192.168.0.1</td>
-                                                    <td>Safary</td>
-                                                    <td>Android 4.0</td>
-                                                    <td>Mobile</td>
-                                                    <td>Mozilla</td>
-                                                    <td>123456789</td>
-                                                    <td>pornovideosite.com</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>03.06.19</td>
+            <td>192.168.0.1</td>
+            <td>Safary</td>
+            <td>Android 4.0</td>
+            <td>Mobile</td>
+            <td>Mozilla</td>
+            <td>123456789</td>
+            <td>pornovideosite.com</td>
 
-                                                </tr>
+        </tr>
 
-                                            </tbody>
-                                        </table>
+    </tbody>
+</table>
 </div>
 </div>
 </div>
