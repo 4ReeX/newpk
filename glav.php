305f15e2028d8c8bc790be9492f544f95d2f3971
4ReeX <56444853+4ReeX@users.noreply.github.com>
2021-06-04 16:45:09 +0300

    fix label color

---
+++ glav.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="utf-8">
+      <meta content="width=device-width,initial-scale=1,maximum-scale=1,shrink-to-fit=no,user-scalable=yes" name="viewport"/>
+      <meta name="apple-mobile-web-app-capable" content="yes"/>
+      <meta name="format-detection" content="telephone=no">
+      <meta name="yandex-verification" content="b90704654f541cce"/>
+      <title>Affimob - Wapclick партнёрская программа, wap подписки.</title>
+      <meta name="description" content="Партнёрская программа Affimob! Партнёрка WapClick подписок - для арбитражников и вебмастеров.">
+      <meta name="keywords" content="партнёрская программа, wap click, wap офферы">
+      <meta property="og:title" content="Affimob - Wapclick партнёрская программа, wap подписки.">
+      <meta property="og:description" content="Партнёрская программа Affimob! Партнёрка WapClick подписок - для арбитражников и вебмастеров.">
+      <meta property="og:image" content="https://affimob.ru/new-logo.svg">
+      <meta property="og:type" content="website">
+      <meta property="og:url" content="https://affimob.ru">
+      <meta property="og:locale" content="ru_RU"/>
+      <meta property="og:site_name" content="Affimob"/>
+      <meta name="twitter:card" content="summary_large_image"/>
+      <meta name="twitter:description" content="Партнёрская программа Affimob! Партнёрка WapClick подписок - для арбитражников и вебмастеров."/>
+      <meta name="twitter:title" content="Affimob - Wapclick партнёрская программа, wap подписки."/>
+      <meta name="twitter:image" content="https://affimob.ru/new-logo.svg"/>
+      <meta name="apple-mobile-web-app-capable" content="yes">
+      <link rel="apple-touch-icon-precomposed" href="/new-logo.png">
+      <link rel="apple-touch-startup-image" href="/startup-logo.png">
+      <link rel="stylesheet" href="assets/css/style.css?111">
+      <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script> 
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+   </head>
+   <body class="morda">
+      
+   </body>
